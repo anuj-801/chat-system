8bd552c775b961a77bf0d402c99b3fedac6c4e1f
anuj-801 <anujchaudhary88844@gmail.com>
2026-08-13 09:37:32 +0530

    creating model(chat)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const PORT = 8080;
+const Chat = require("./models/chat");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -16,6 +17,17 @@ main()
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
+let chat1 = new Chat({
+    from: "anuj",
+    to: "ram",
+    msg: "send me your code",
+    created_at: new Date()
+});
+
+chat1.save()
+.then((res) => {
+    console.log(res);
+});
 
 app.get("/", (req, res) => {
     res.send("Working");
