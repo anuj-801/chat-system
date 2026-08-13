27b205cfd8d91c015643aa87297457a2094db81d
anuj-801 <anujchaudhary88844@gmail.com>
2026-08-13 10:58:38 +0530

    update message

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const mongoose = require("mongoose");
 const path = require("path");
 const PORT = 8080;
 const Chat = require("./models/chat");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 main()
     .then(() => {
@@ -48,6 +50,20 @@ app.post("/chats", (req, res) => {
         .catch((err) => {
             console.log(err);
         });
+    res.redirect("/chats");
+});
+
+app.get("/chats/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", {chat});
+});
+
+app.put("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    let {msg: newMsg} = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(id, {msg: newMsg}, {runValidators: true, new: true});
+
     res.redirect("/chats");
 });
 
