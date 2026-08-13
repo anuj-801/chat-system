cb222806fe5e03ba691ed877f4df874771496974
anuj-801 <anujchaudhary88844@gmail.com>
2026-08-13 10:31:23 +0530

    new chat insertion dynamically

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const Chat = require("./models/chat");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
 main()
     .then(() => {
@@ -26,6 +27,28 @@ app.get("/", (req, res) => {
 app.get("/chats", async (req, res) => {
     let chats = await Chat.find();
     res.render("index.ejs", { chats });
+});
+
+app.get("/chats/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.post("/chats", (req, res) => {
+    let { from, msg, to } = req.body;
+    let newChat = new Chat({
+        from: from,
+        to: to,
+        msg: msg,
+        created_at: new Date()
+    });
+    newChat.save()
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    res.redirect("/chats");
 });
 
 app.listen(PORT, () => {
